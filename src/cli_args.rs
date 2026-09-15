c9134c4faaa2caaf40185694e11eda7221c06dce
Mitchell Wright <36946520+Pseudooo@users.noreply.github.com>
2026-09-15 22:11:34 +0100

    Writing groupings to output (#7)

--- src/cli_args.rs
+++ src/cli_args.rs
@@ -1,11 +1,14 @@
-use std::path::PathBuf;
 use clap_derive::Parser;
+use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct CliArgs {
     #[arg(short, long, required = false, default_value = "./")]
     pub target: PathBuf,
+
+    #[arg(short, long, required = false, default_value = "./output")]
+    pub output: PathBuf,
 }
 
 impl CliArgs {
@@ -16,6 +19,10 @@ impl CliArgs {
 
         if !self.target.is_dir() {
             return Err("Target is not a directory, can't deduplicate a single file???".to_string());
+        }
+
+        if self.output.exists() && !self.output.is_dir() {
+            return Err("Output path must be a directory".to_string());
         }
 
         Ok(())
